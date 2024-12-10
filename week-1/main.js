67be2dbfdefeac48ea1a7beee91300a25b9d3cf0
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2024-12-10 23:20:58 +0530

    add more

--- week-1/main.js
+++ week-1/main.js
@@ -1,24 +1,34 @@
-// week 1.2
-// JS Foundation
-// assignment questions 3
+// * week 1.2
+// * JS Foundation
 
-const allUsers = [
-  {
-    firstName: "Ganesham",
-    gender: "Male",
-  },
-  {
-    firstName: "gandu",
-    gender: "Male",
-  },
-  {
-    firstName: "roshni",
-    gender: "female",
-  },
-];
+// todo assignment questions 3
 
-for (let i = 0; i < allUsers.length; i++) {
-  if (allUsers[i]["gender"] === "Male") {
-    console.log(allUsers[i]["firstName"]);
-  }
+// const allUsers = [
+//   {
+//     firstName: "Ganesham",
+//     gender: "Male",
+//   },
+//   {
+//     firstName: "gandu",
+//     gender: "Male",
+//   },
+//   {
+//     firstName: "roshni",
+//     gender: "female",
+//   },
+// ];
+
+// for (let i = 0; i < allUsers.length; i++) {
+//   if (allUsers[i]["gender"] === "Male") {
+//     console.log(allUsers[i]["firstName"]);
+//   }
+// }
+
+// ? functions in js
+
+function sum(a, b) {
+  return a + b;
 }
+
+const value = sum(2, 3);
+console.log(value);
