2342ae3af4058cf9f1d3bc6a9242a2d60c73ede8
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2025-01-15 23:56:42 +0530

    JS conecpt

---
+++ week-1/gist.js
@@ -0,0 +1,15 @@
+function sum(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+
+function displayResult() {
+  console.log("Result of the sum is:" + data);
+}
+
+function displayResultPassive(data) {
+  console.log("Result of the sum is:" + data);
+}
+
+//* you are allowed to call one function from another function or after the function declaration
+//* how you will you display the result of the sum function in the console?
--- week-1/main.js
+++ week-1/main.js
@@ -26,9 +26,20 @@
 
 // ? functions in js
 
-function sum(a, b) {
-  return a + b;
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const value = sum(2, 3);
+// console.log(value);
+
+// ? bounty question :- why is this code taking so long to execute?
+
+let sum = 0;
+for (let i = 0; i < 100000000000; i++) {
+  sum = sum + i;
 }
 
-const value = sum(2, 3);
-console.log(value);
+console.log(sum);
+
+//* answer :-  the code is taking so long to execute because the loop is running 100000000000 times and the sum is being calculated for each iteration. This is a very large number of iterations and the sum is being calculated for each iteration, which is why it is taking so long to execute.
