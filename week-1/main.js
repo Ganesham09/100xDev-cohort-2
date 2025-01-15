8eb7eb7e5941557953e596aafde793b5cb14f8f2
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2025-01-16 00:33:29 +0530

    JS concepts

--- week-1/main.js
+++ week-1/main.js
@@ -1,29 +1,6 @@
 // * week 1.2
 // * JS Foundation
 
-// ? assignment questions 3
-
-// const allUsers = [
-//   {
-//     firstName: "Ganesham",
-//     gender: "Male",
-//   },
-//   {
-//     firstName: "gandu",
-//     gender: "Male",
-//   },
-//   {
-//     firstName: "roshni",
-//     gender: "female",
-//   },
-// ];
-
-// for (let i = 0; i < allUsers.length; i++) {
-//   if (allUsers[i]["gender"] === "Male") {
-//     console.log(allUsers[i]["firstName"]);
-//   }
-// }
-
 // ? functions in js
 
 // function sum(a, b) {
@@ -35,11 +12,36 @@
 
 // ? bounty question :- why is this code taking so long to execute?
 
-let sum = 0;
-for (let i = 0; i < 100000000000; i++) {
-  sum = sum + i;
-}
+// let sum = 0;
+// for (let i = 0; i < 100000000000; i++) {
+//   sum = sum + i;
+// }
 
-console.log(sum);
+// console.log(sum);
 
 //* answer :-  the code is taking so long to execute because the loop is running 100000000000 times and the sum is being calculated for each iteration. This is a very large number of iterations and the sum is being calculated for each iteration, which is why it is taking so long to execute.
+
+//? random code
+
+function calculateAirthmetic(a, b, type) {
+  if (type == "sum") {
+    const value = sum(a, b);
+    return value;
+  }
+
+  if (type == "minus") {
+    const value = minus(a, b);
+    return value;
+  }
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+const value = calculateAirthmetic(2, 3, "minus"); //* define the type of operation you want to perform
+console.log(value);
